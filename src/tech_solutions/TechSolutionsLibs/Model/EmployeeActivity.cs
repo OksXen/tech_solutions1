6e500a21ebbf8c88537a45f659ea6f3a95503b1b
Ferdinand Codinera <fcodinera@outlook.com>
2019-11-16 16:06:31 -0500

    added EmployeeActivity class

---
+++ src/tech_solutions/TechSolutionsLibs/Model/EmployeeActivity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechSolutionsLibs.Model
+{
+    public partial  class EmployeeActivity
+    {
+        [Key]
+        public int ActivityId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string ActivityName { get; set; }
+        public string Comments { get; set; }
+    }
+}

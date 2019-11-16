06f56002b080ebc1bb132acc09a3c8574d91941b
Ferdinand Codinera <fcodinera@outlook.com>
2019-11-16 16:07:56 -0500

    Added Provider Class

---
+++ src/tech_solutions/TechSolutionsLibs/Provider/ActivityDBContext.cs
@@ -0,0 +1,48 @@
+﻿using TechSolutionsLibs.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechSolutionsLibs.Provider
+{
+    public partial class ActivityDBContext : DbContext
+    {
+        public virtual DbSet<EmployeeActivity> EmployeeActivity { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer(@"Data Source=tcp:s19.winhost.com;Initial Catalog=DB_127750_activity;User ID=DB_127750_activity_user;Password=th8uxgWBLf8M;Integrated Security=False;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeActivity>(entity =>
+            {
+
+                entity.Property(e => e.FirstName)
+                   .HasMaxLength(50)
+                   .IsUnicode(false);                
+
+                entity.Property(e => e.LastName)
+                   .HasMaxLength(50)
+                   .IsUnicode(false);
+
+                entity.Property(e => e.EmailAddress)
+                  .HasMaxLength(50)
+                  .IsUnicode(false);
+
+                entity.Property(e => e.ActivityName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Comments)
+                   .HasMaxLength(1000)
+                   .IsUnicode(false);
+            });
+
+           
+        }
+    }
+}
---
+++ src/tech_solutions/TechSolutionsLibs/Provider/EmployeeActivityProvider.cs
@@ -0,0 +1,43 @@
+﻿using TechSolutionsLibs.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TechSolutionsLibs.Provider
+{
+    public class EmployeeActivityProvider
+    {
+        ActivityDBContext activityDBContext;
+
+        public EmployeeActivityProvider(ActivityDBContext dBContext)
+        {
+            activityDBContext = dBContext;
+        }
+
+        public int AddEmployee(EmployeeActivity employeeActivity)
+        {
+            try
+            {
+                activityDBContext.EmployeeActivity.Add(employeeActivity);
+                activityDBContext.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<EmployeeActivity> GetEmployeeActivities()
+        {
+            try
+            {
+                return activityDBContext.EmployeeActivity.ToList();
+                                
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}

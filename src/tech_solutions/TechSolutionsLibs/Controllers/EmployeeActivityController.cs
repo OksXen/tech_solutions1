7eeca830568cccbca4b6876c1bc21be1aa424b4b
Ferdinand Codinera <fcodinera@outlook.com>
2019-11-16 16:10:22 -0500

    Added Employee Activity Controller

---
+++ src/tech_solutions/TechSolutionsLibs/Controllers/EmployeeActivityController.cs
@@ -0,0 +1,35 @@
+﻿using TechSolutionsLibs.Helper;
+using TechSolutionsLibs.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechSolutionsLibs.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class EmployeeActivityController
+    {
+        public EmployeeActivityController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<EmployeeActivity> Get()
+        {
+
+            return EmployeeActivityHelper.GetEmployeeActivities().ToArray();
+
+            
+        }
+
+        [HttpPost]
+        [Route("AddEmployeeActivity")]
+        public int AddEmployeeActivity([FromBody] EmployeeActivity employeeActivity)
+        {
+            return EmployeeActivityHelper.AddEmployeeActivity(employeeActivity);
+        }
+    }
+}

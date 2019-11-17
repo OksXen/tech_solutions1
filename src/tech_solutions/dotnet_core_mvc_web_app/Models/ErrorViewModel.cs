659e10d79cc4f5fe338eab31a8ba61643acb9fb2
Ferdinand Codinera <fcodinera@outlook.com>
2019-11-17 15:18:38 -0500

    added dotnet_core_mvc_web_app project

---
+++ src/tech_solutions/dotnet_core_mvc_web_app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_core_mvc_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

1ca1399822f50bfbb413c12987d73a55cc951287
Ferdinand Codinera <fcodinera@outlook.com>
2019-11-17 19:23:55 -0500

    added dapper respository and unit test

---
+++ src/tech_solutions/TechSolutionsLibs/Repository/EmployeeActivityByDapperRepository.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSolutionsLibs.Models;
+using TechSolutionsLibs.Repository.Interface;
+
+
+
+namespace TechSolutionsLibs.Repository
+{
+    public class EmployeeActivityByDapperRepository: IEmployeeActivityRepository
+    {
+        IDBSettings _dBSettings;
+
+        public EmployeeActivityByDapperRepository(IDBSettings dBSettings)
+        {
+            _dBSettings = dBSettings;
+        }
+
+        public async Task<int> AddEmployee(EmployeeActivity employeeActivity)
+        {
+            try
+            {
+
+                if (ReferenceEquals(employeeActivity, null)) throw new Exception("employeeActivity is null");
+
+                var connectionString = _dBSettings.ConnectionString;
+                var sql = "INSERT INTO EmployeeActivity VALUES(@fName, @lname, @email, @aname, @comments);SELECT CAST(SCOPE_IDENTITY() as int) ";
+                var id = 0;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    var result = connection.Query<int>(sql, new { @fName= employeeActivity.FirstName, 
+                        @lname= employeeActivity.LastName, 
+                        @email= employeeActivity.EmailAddress, 
+                        @aname= employeeActivity.ActivityName, @comments= employeeActivity.Comments
+                    });                    
+                  
+                    if (result.Any()) id = (int)result.Single();
+                    
+                }
+
+                return id;
+            }
+            catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+
+
+        public IEnumerable<EmployeeActivity> GetEmployeeActivities()
+        {
+            try
+            {
+
+                var connectionString = _dBSettings.ConnectionString;
+                IEnumerable<EmployeeActivity> employeeActivities;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    employeeActivities = connection.Query<EmployeeActivity>("select * from EmployeeActivity order by  activityid desc");
+                }
+
+                if (ReferenceEquals(employeeActivities, null)) throw new Exception("employeeActivities is null.");
+
+                return employeeActivities;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+
+        }
+    }
+}
---
+++ src/tech_solutions/XUnitTestProject1/EmployeeActivityDapperProviderTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using TechSolutionsLibs.Models;
+using TechSolutionsLibs.Repository;
+using TechSolutionsLibs.Repository.Interface;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class EmployeeActivityDapperProviderTest
+    {
+        /// <summary>
+        /// ensures add employee returns identity id
+        /// </summary>
+        [Fact]
+        public async void AddEmployeeTest()
+        {
+            //arrange
+            IDBSettings dBSettings = new DBSettings();
+            
+            EmployeeActivityByDapperRepository employeeActivityProvider = new EmployeeActivityByDapperRepository(dBSettings);
+
+            var tics = DateTime.Now.Ticks;
+            EmployeeActivity employeeActivity = new EmployeeActivity()
+            {
+                FirstName = string.Concat("dapper_", tics),
+                LastName = string.Concat("LName_", tics),
+                EmailAddress = string.Concat(tics, "@email.com"),
+                ActivityName = string.Concat("Activity_Name_", tics),
+                Comments = string.Concat("Comments_", tics)
+            };
+
+            //act
+            var result = await employeeActivityProvider.AddEmployee(employeeActivity);
+
+            //assert
+            Assert.NotEmpty(dBSettings.ConnectionString);
+            Assert.NotNull(employeeActivity);
+            Assert.NotEqual<int>(0, result);
+        }
+
+
+
+        /// <summary>
+        /// ensures get employ activities returns values and not null
+        /// </summary>
+        [Fact]
+        public void GetEmployeeActivitiesTest()
+        {
+            //arrange
+            IDBSettings dBSettings = new DBSettings();
+            EmployeeActivityByDapperRepository employeeActivityProvider = new EmployeeActivityByDapperRepository(dBSettings);
+
+            //act
+            var result = employeeActivityProvider.GetEmployeeActivities().ToList();
+
+            //assert            
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
